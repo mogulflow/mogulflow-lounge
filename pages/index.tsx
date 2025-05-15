44ed5c0c72efe5c5ca271ab62157020c36942879
mogulflow <mogulflow@gmail.com>
2025-05-15 22:07:44 +0200

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Welcome to the MogulFlow AI Lounge!</h1>
+      <p>Our AI characters will be with you shortly...</p>
+    </div>
+  );
+}
